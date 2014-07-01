ef3f936c1b8bc4f44604e7225239115e0641717f
ernestjumbe <e2jeyy@gmail.com>
2014-07-01 14:59:12 +0200

    Added django debug toolbar settings

--- project_name/settings/local.py
+++ project_name/settings/local.py
@@ -22,6 +22,8 @@ DATABASES = {
 # Application definition
 
 INSTALLED_APPS += (
+	'debug_toolbar',
+	'django_extensions',
 )
 
 #==============================================================================
@@ -35,6 +37,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #==============================================================================
 
 MIDDLEWARE_CLASSES += (
+	'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 #==============================================================================
@@ -44,3 +47,24 @@ MIDDLEWARE_CLASSES += (
 #==============================================================================
 # Third party app settings
 #==============================================================================
+
+#Django Debug Toolbar settings
+#=============================
+
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
